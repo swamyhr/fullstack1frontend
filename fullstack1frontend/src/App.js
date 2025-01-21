c9f93e045dc2c4e9f5ef52a6254f2da9542d33cf
swamyhr <swamyrangannanayaka@gmail.com>
2025-01-21 22:51:44 +0530

    inital setup of react app , installed bootstrap for styling

---
+++ fullstack1frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
