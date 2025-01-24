880ffc963f2773299addccad0fcec0fc8e5565e1
swamyhr <swamyrangannanayaka@gmail.com>
2025-01-24 22:42:15 +0530

    basic setup for routing , replaced axios with fetch temporarily

--- fullstack1frontend/src/pages/Home.js
+++ fullstack1frontend/src/pages/Home.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import api from '../services/api'
+import React, {useEffect, useState} from 'react'
+// import api from '../services/api'
 
 function Home() {
 
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=> {
+        initialLoadUsers();
+    }, [])
 
     const initialLoadUsers = async () => {
-        const users = await api.get("users", 
+        // const users = await api.get("/users", 
           
-        ).then(response => {
-            console.log("Data received ", response.data)
-        })
-        console.log("Users from API ", users.data);
+        // ).then(response => {
+        //     console.log("Data received ", response.data)
+        // })
+        // console.log("Users from API ", users.data);
+        try {
+            const result = await fetch("http://localhost:8080/users");
+            const response = await result.json();
+            console.log("response ", response);
+            
+            // console.log("Data ", data);
+            setUsers(response);
+        } catch (error) {
+            console.log("Error ", error);
+        }
+        
     }
-
-    initialLoadUsers();
 
   return (
     <div className='container'>
@@ -23,18 +37,24 @@ function Home() {
                 <thead>
                     <tr>
                         <th scope='column'>#</th>
-                        <th scope='column'>First</th>
-                        <th scope='column'>Second</th>
+                        <th scope='column'>Name</th>
+                        <th scope='column'>User Name</th>
                         <th scope='column'>Handle</th>
+                        <th scope="Column">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope='row'>1</th>
-                        <td>First Name</td>
-                        <td>Second Name</td>
-                        <td>Handle@firstsecond</td>
-                    </tr>
+                    {users.length ?  users.map((user, index) => (<tr key={user.id}>
+                        <th scope='row'>{index +1}</th>
+                        <td>{user.name}</td>
+                        <td>{user.userName}</td>
+                        <td>{user.email}</td>
+                        <td>
+                            <button className='btn btn-primary mx-2'>View </button>
+                            <button className='btn btn-outline-primary mx-2'>Edit </button>
+                            <button className='btn btn-danger mx-2'>Delete</button>
+                        </td>
+                    </tr>)) : <td>No users found</td>}
                 </tbody>
             </table>
 
--- fullstack1frontend/src/services/api.js
+++ fullstack1frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "http://localhost:8080",
     headers: {
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "http://localhost:8080"
     } 
 })
